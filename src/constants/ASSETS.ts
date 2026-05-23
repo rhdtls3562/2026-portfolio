ddcc56658505fd99d8736ad62c56b1803e8dca5d
권새롬 <rhdtls3562@naver.com>
2026-05-23 16:33:01 +0900

    🐛 Fix: ASSETS·NAVER_MAIL 플랫 파일 복원 - Vercel 빌드 오류 수정

---
+++ src/constants/ASSETS.ts
@@ -0,0 +1,33 @@
+/** public 에셋 경로를 한 곳에서 관리하는 공용 상수 파일. */
+export const icons = {
+  iconEarth: "/asset/icon/icon_earth.png",
+  iconFile: "/asset/icon/icon_file.png",
+  iconFolder: "/asset/icon/icon_folder.svg",
+  iconMail: "/asset/icon/icon_mail.png",
+  iconMore: "/asset/icon/icon_more.png",
+  iconHello: "/asset/icon/icon_hello.svg",
+} as const;
+
+export const images = {
+  imgButtonOff: "/asset/img/img_button_off.jpg",
+  imgButtonOn: "/asset/img/img_button_on.jpg",
+  imgDesktop: "/asset/img/img_desktop.png",
+  imgMain: "/asset/img/img_main.jpg",
+  imgProfile: "/asset/img/img_profile.png",
+  imgProfileBlack: "/asset/img/img_profile_black.png",
+  imgBts: "/asset/img/img_beautyst.png",
+  imgCoworkers: "/asset/img/img_coworkers.png",
+  imgCoworkersBrowser: "/asset/img/img_coworkers_browser.png",
+  imgTaskifyBrowser: "/asset/img/img_taskify_browser.png",
+  imgBtsBrowser: "/asset/img/img_bts_browser.png",
+  imgPrBrowser: "/asset/img/img_pr_browser.jpg",
+  imgDangBrowser: "/asset/img/img_dang_browser.jpg",
+  imgSevenBrowser: "/asset/img/img_seven_browser.jpg",
+  imgSyBrowser: "/asset/img/img_sy_browser.jpg",
+  imgSuBrowser: "/asset/img/img_su_browser.jpg",
+} as const;
+
+export const documents = {
+  chevronRecommendationPdf: "/asset/pdf/쉐브론_권새롬_추천서 (1).pdf",
+  recommendationPdf: "/asset/pdf/kwon-saerom-recommendation.pdf",
+} as const;
